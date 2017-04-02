b0ab799a597076da83b6038a4696934428bd0f15
Chetas Rao <thephantomblu@gmail.com>
2017-04-03 00:26:10 +0530

    next

--- app.py
+++ app.py
@@ -15,15 +15,15 @@ def execute(msg, sender):
     print msg
     if (msg[0] in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']) and (msg[1] in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']):
         sender_db[sender] = {'section': msg[0], 'group': msg[1]}
+        return "You\'re now in the database!"
+        
     if sender in sender_db.keys():
         section = sender_db[sender]['section']
         group = sender_db[sender]['group']
-
         return "You are in section " + section + ' and practical group ' + group
 
     else:
         return "Enter section and group in this format: \"S G\""
-
 
 @app.route('/', methods=['GET'])
 def verify():
