bfb5620cc4f55bc3ba8261ddf30a09f8b20aad97
Chetas Rao <thephantomblu@gmail.com>
2017-04-03 00:11:43 +0530

    next

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ sender_db = pickle.load(open('database.db', 'rb'))
 def execute(msg, sender):
     msg = msg.split()
     print msg
-    if (msg[0][0] in 'ABCDE') and (msg[1][0] in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']):
+    if (msg[0] in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']) and (msg[1] in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']):
         sender_db[sender] = {section: msg[0], group: msg[1]}
     if sender in sender_db.keys():
         section = sender_db[sender][section]
