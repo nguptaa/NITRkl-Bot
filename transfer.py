016d0cecd059f161220d52198d1a0e38e3df4664
guptan <nguptaa183@gmail.com>
2017-04-22 09:57:12 +0530

    fixes

--- transfer.py
+++ transfer.py
@@ -8,7 +8,7 @@ def uploadFile(filename):
             return res.text
         except Exception as e:
             print e
-            return ''
+            return ' '
 
 def downloadFile(url, filename):
     r = requests.get(url, stream=True)
